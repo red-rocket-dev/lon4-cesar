60fc2810c86109ab9407352fc332ca5b0017d1d9
Jakub <kubplon@gmail.com>
2020-05-24 14:09:37 +0200

    example

--- src/pl/sda/Main.java
+++ src/pl/sda/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// szyfr cezara - implementacja
+        String toEncode = "123";
+        char ch = 'a';
+        int afterShift = ch + 3;
+        char afterShiftChar = (char) afterShift;
+        System.out.println(afterShiftChar);
+        int shift = 10;
     }
 }
