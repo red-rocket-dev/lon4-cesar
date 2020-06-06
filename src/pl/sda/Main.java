a5d336b4421a831c215b9ae8a341c77660c8c6fe
Jakub <kubplon@gmail.com>
2020-06-06 09:37:45 +0200

    solution

--- src/pl/sda/Main.java
+++ src/pl/sda/Main.java
@@ -3,12 +3,65 @@ package pl.sda;
 public class Main {
 
     public static void main(String[] args) {
-	// szyfr cezara - implementacja
-        String toEncode = "123";
-        char ch = 'a';
-        int afterShift = ch + 3;
-        char afterShiftChar = (char) afterShift;
-        System.out.println(afterShiftChar);
-        int shift = 10;
+        // szyfr cezara - implementacja
+        int shift = 3;
+        String encoded = encode("AbCd", shift);
+        System.out.println(encoded);
+        System.out.println(decrypt(encoded, 3));
     }
+
+    public static String encode(String word, int shift) {
+        StringBuilder encodedSb = new StringBuilder();
+        char[] table = word.toCharArray();
+        for (int i = 0; i < table.length; i++) {
+            char letter = table[i];
+            int ascii = letter;
+            int afterShift = ascii + shift;
+            char letterAfterShift = (char) afterShift;
+            encodedSb.append(letterAfterShift);
+        }
+        return encodedSb.toString();
+    }
+
+    public static String decrypt(String encodedWord, int shift) {
+        StringBuilder encodedSb = new StringBuilder();
+        char[] table = encodedWord.toCharArray();
+        for (int i = 0; i < table.length; i++) {
+            char letter = table[i];
+            int ascii = letter;
+            int afterShift = ascii - shift;
+            char letterAfterShift = (char) afterShift;
+            encodedSb.append(letterAfterShift);
+        }
+        return encodedSb.toString();
+    }
+
+    public static String decryptAlphabet(String cipherText, int shift) {
+        String plainText = "";
+        int length = cipherText.length();
+        for (int i = 0; i < length; i++) {
+            char ch = cipherText.charAt(i);
+            if (Character.isLetter(ch)) {
+                if (Character.isLowerCase(ch)) {
+                    char c = (char) (ch - shift);
+                    if (c < 'a') {
+                        plainText += (char) (ch + (26 - shift));
+                    } else {
+                        plainText += c;
+                    }
+                } else if (Character.isUpperCase(ch)) {
+                    char c = (char) (ch - shift);
+                    if (c < 'A') {
+                        plainText += (char) (ch - (26 - shift));
+                    } else {
+                        plainText += c;
+                    }
+                }
+            } else {
+                plainText += ch;
+            }
+        }
+        return plainText;
+    }
+
 }
